2153a4bf87195ede1f7dd59f2dba38fd06aaec60
kvsiva <sivakrishna.konijeti@gmail.com>
2021-12-12 22:47:43 +0530

    updated package name for main class

--- src/main/java/com/sv/controller/StudentController.java
+++ src/main/java/com/sv/controller/StudentController.java
@@ -1,6 +1,6 @@
 package com.sv.controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/student")
 public class StudentController {
 
-    @PostMapping("/ping")
+    @GetMapping("/ping")
     public void ping(){
         System.out.println("Hello World");
     }
--- src/main/java/com/sv/entity/StudentInfo.java
+++ src/main/java/com/sv/entity/StudentInfo.java
@@ -1,12 +1,17 @@
 package com.sv.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.io.Serializable;
 
 @Data
 @Entity
-public class StudentInfo {
+@AllArgsConstructor
+@NoArgsConstructor
+public class StudentInfo implements Serializable {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
